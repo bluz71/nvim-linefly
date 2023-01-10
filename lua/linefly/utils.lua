d0e0b124eaa9f6911bc38f257d43014f29dbb7ab
Dennis B <bluz71@users.noreply.github.com>
2023-01-10 15:34:32 +1100

    Use API aliases

--- lua/linefly/utils.lua
+++ lua/linefly/utils.lua
@@ -1,14 +1,19 @@
+local buf_get_option = vim.api.nvim_buf_get_option
+local tabpage_list_wins = vim.api.nvim_tabpage_list_wins
+local win_get_buf = vim.api.nvim_win_get_buf
+local win_get_config = vim.api.nvim_win_get_config
+
 local M = {}
 
 -- Return the number of split windows exluding floating windows and other
 -- special windows.
 M.window_count = function()
   local count = 0
-  local windows = vim.api.nvim_tabpage_list_wins(0)
+  local windows = tabpage_list_wins(0)
 
-  for _, v in pairs(windows) do
-    local cfg = vim.api.nvim_win_get_config(v)
-    local ft = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(v), "filetype")
+  for _, w in pairs(windows) do
+    local cfg = win_get_config(w)
+    local ft = buf_get_option(win_get_buf(w), "filetype")
 
     if (cfg.relative == "" or cfg.external == false) and ft ~= "qf" then
       count = count + 1
