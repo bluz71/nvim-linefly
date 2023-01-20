bb0bcced2c2bac2b4332436f46934ab34fd9df62
Dennis B <bluz71@users.noreply.github.com>
2023-01-20 17:55:01 +1100

    Simplified options initialization

--- lua/linefly/options.lua
+++ lua/linefly/options.lua
@@ -1,32 +1,31 @@
-local options_list = {
-  separator_symbol = "⎪",
-  progress_symbol = "↓",
-  active_tab_symbol = "▪",
-  git_branch_symbol = "",
-  error_symbol = "E",
-  warning_symbol = "W",
-  information_symbol = "I",
-  tabline = false,
-  winbar = false,
-  with_file_icon = true,
-  with_git_branch = true,
-  with_git_status = true,
-  with_diagnostic_status = true,
-  with_session_status = true,
-  with_indent_status = false,
-}
-
-local user_settings_merged = false
+local g = vim.g
+local options_initialized = false
 
 local M = {}
 
 M.list = function()
-  if not user_settings_merged then
-    options_list = vim.tbl_extend("force", options_list, vim.g.linefly_options or {})
-    user_settings_merged = true
+  if not options_initialized then
+    g.linefly_options = {
+      separator_symbol = g.linefly_options.separator_symbol or "⎪",
+      progress_symbol = g.linefly_options.progress_symbol or "↓",
+      active_tab_symbol = g.linefly_options.active_tab_symbol or "▪",
+      git_branch_symbol = g.linefly_options.git_branch_symbol or "",
+      error_symbol = g.linefly_options.error_symbol or "E",
+      warning_symbol = g.linefly_options.warning_symbol or "W",
+      information_symbol = g.linefly_options.information_symbol or "I",
+      tabline = g.linefly_options.tabline or false,
+      winbar = g.linefly_options.winbar or false,
+      with_file_icon = g.linefly_options.with_file_icon or true,
+      with_git_branch = g.linefly_options.with_git_branch or true,
+      with_git_status = g.linefly_options.with_git_status or true,
+      with_diagnostic_status = g.linefly_options.with_diagnostic_status or true,
+      with_session_status = g.linefly_options.with_session_status or true,
+      with_indent_status = g.linefly_options.with_indent_status or false,
+    }
+    options_initialized = true
   end
 
-  return options_list
+  return g.linefly_options
 end
 
 return M
